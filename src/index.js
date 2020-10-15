1bb0ebc8902bcd5b82521e2ed724034d353f4a00
rmiglin <rossmiglin@gmail.com>
2020-10-14 23:08:57 -0400

    clean paddle logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import "./styles/index.scss";
 
+
+// create block class 
+// create paddle class 
+// create p+b class (new collision rules)
+// play game function
+//   -game state of falling objects (iterate through obj array and move them down)
+// user class (health, current score)
+// firebase -- high score
+
+
 // canvas variables
 var canvas = document.getElementById('canvas');
 const ctx = canvas.getContext("2d");
@@ -7,14 +17,15 @@ const ctx = canvas.getContext("2d");
 // ball variables
 var x = Math.floor(Math.random() * canvas.width) + 1;
 var y = canvas.height - 200;
-var dx = 0;
-var dy = 2;
+const ball_radius = 10;
+var dy = 4;
 
 // paddle variables
 var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth)/2;
-
+var paddleLeftX = paddleX - paddleWidth/2;
+var paddleRightX = paddleX + paddleWidth/2;
 var rightPressed = false;
 var leftPressed = false;
 
@@ -37,19 +48,18 @@ function keyUpHandler(e) {
   }
 }
 
-
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, 10, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = "black";
     ctx.fill();
     ctx.closePath();
 }
 
 function drawPaddle() {
     ctx.beginPath();
-    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
+    ctx.rect(paddleLeftX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "black";
     ctx.fill();
     ctx.closePath();
 }
@@ -63,22 +73,35 @@ function draw() {
         x = Math.floor(Math.random() * canvas.width) + 1;
         y = canvas.height - 200;
     }
+
+    if (y + ball_radius === canvas.height - paddleHeight && (x >= paddleLeftX && x <= paddleRightX)){
+        x = paddleX;
+    } else {   
+        y += dy;
+    }
+
     if (rightPressed) {
         paddleX += 7;
-        if (paddleX + paddleWidth > canvas.width) {
-            paddleX = canvas.width - paddleWidth;
+        if (paddleRightX >= canvas.width) {
+            paddleX = canvas.width - paddleWidth/2;
         }
     } else if (leftPressed) {
         paddleX -= 7;
-        if (paddleX < 0) {
-            paddleX = 0;
+        if (paddleLeftX <= 0) {
+            paddleX = paddleWidth/2;
         }
     }
-    x += dx;
-    y += dy;
+    paddleLeftX = paddleX - paddleWidth / 2;
+    paddleRightX = paddleX + paddleWidth / 2;
+    // console.log(`x is: ${x}`);
+    // console.log(`y is: ${y}`);
+    // console.log(`paddleX is ${paddleX}`);
+    // console.log(`paddleLeftX is ${paddleLeftX}`);
+    // console.log(`paddleRightX is ${paddleRightX}`);
+    
 }
 
-setInterval(draw, 10);
+setInterval(draw, 30);
 
 console.log("canvas is running");
 
