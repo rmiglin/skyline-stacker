4475f6ceec43690c80c3cbf374345668ed2ab726
rmiglin <rossmiglin@gmail.com>
2020-11-11 02:28:44 -0500

    add working music player:

--- src/index.js
+++ src/index.js
@@ -7,8 +7,17 @@ var totalSeconds = 300;
 
 function setTime() {
   totalSeconds -= 1;
-  secondsLabel.innerHTML = totalSeconds % 60;
-  minutesLabel.innerHTML = parseInt(totalSeconds / 60);
+  secondsLabel.innerHTML = pad(totalSeconds % 60);
+  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+  var valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
 }
 
 // create block class 
@@ -22,6 +31,7 @@ let game = new Game();
 var start = document.getElementById("start");
 
 start.onclick = function(){
+  audio.play();
   game.play();
   setInterval(setTime, 1000);
 };
@@ -41,6 +51,48 @@ close.onclick = function() {
 window.onclick = function(e) {
   if (e.target == modal) {
     modal.style.display = "none";
+  }
+}
+
+var audio = document.getElementById("audio-player");
+var audio_play = document.getElementById("audio-play");
+var audio_pause = document.getElementById("audio-pause");
+var audio_lower = document.getElementById("audio-lower");
+var audio_increase = document.getElementById("audio-increase");
+
+audio_play.onclick = function(){
+  playAudio();
+}
+
+audio_pause.onclick = function(){
+  pauseAudio();
+}
+
+audio_lower.onclick = function(){
+  lowerAudio();
+}
+
+audio_increase.onclick = function(){
+  increaseAudio();
+}
+
+function playAudio() {
+  audio.play();
+}
+
+function pauseAudio() {
+  audio.pause();
+}
+
+function lowerAudio() {
+  if(audio.volume >= 0){
+    audio.volume -= 0.1;
+  }
+}
+
+function increaseAudio() {
+  if(audio.volume < 1){
+    audio.volume += 0.1;
   }
 }
 
